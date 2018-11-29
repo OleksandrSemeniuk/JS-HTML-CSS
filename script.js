d86e2d12f2b3fc31d83210fc39abd9440a1dcd3f
OS <semenyukom@gmail.com>
2018-11-29 11:20:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+var name1 = "Вася"; name2 = "Петя"; name3 = "Стас";
+var cond1 = false; cond2 = true; cond3 = false;
+if (cond1) {
+  console.log ("Привет, " + name1);
+} else if (cond2) {
+  console.log ("Привет, " + name2);
+} else if (cond3) {
+  console.log ("Привет, " + name3);
+} else {
+  console.log ("Я тебя не знаю!")
+}
+var res = document.getElementById("qq");
+console.log (qq);
 
+
+// var qq = document.getElementById('qq');
+//   console.log (qq);
+
+// var sheepCounted = 0;
+// while (sheepCounted < 10) {
+  
+//   qq.textContent = "Овец посчитано: " + sheepCounted + "!";
+//   console.log ("Овец посчитано: " + sheepCounted + "!")
+//   sheepCounted ++
+// }
